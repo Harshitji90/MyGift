81d6c5de09ceb9407bfc31da1ad93a61361a03d6
Harshit Bisen <harshitbisen70@gmail.com>
2026-08-29 11:13:03 +0530

    latest

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const app = require("./backend/app")
+// const app = require("./backend/app")
+// require("dotenv").config();
+
+// const ConnectDb=require('./backend/db/db')
+// const dns = require("dns");
+// dns.setServers(['1.1.1.1'],['8.8.8.8']);
+// ConnectDb();
+
+// app.listen(3000,()=>{
+//     console.log("Sever is running on PORT 3000")//
+// })
+
+const app = require("./backend/app");
+
 require("dotenv").config();
 
-const ConnectDb=require('./backend/db/db')
+const ConnectDb = require("./backend/db/db");
+
 const dns = require("dns");
-dns.setServers(['1.1.1.1'],['8.8.8.8']);
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 ConnectDb();
 
-app.listen(3000,()=>{
-    console.log("Sever is running on PORT 3000")//
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
