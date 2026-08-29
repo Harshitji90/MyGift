ee91e8a00d97c707a7eb52bc1e8c4305d6087705
Harshit Bisen <harshitbisen70@gmail.com>
2026-08-29 11:07:11 +0530

    init

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ dns.setServers(['1.1.1.1'],['8.8.8.8']);
 ConnectDb();
 
 app.listen(3000,()=>{
-    console.log("Sever is running on PORT 3000")
+    console.log("Sever is running on PORT 3000")//
 })
